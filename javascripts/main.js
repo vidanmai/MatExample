3599a374eb1db1b7d3029c5ac2c566e95c93414c
vidanmai <vidanmai@gmail.com>
2018-07-25 22:01:39 +0200

    Initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,11 @@
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+const dropDown = document.querySelectorAll('.dropdown-trigger');
+M.Dropdown.init(dropDown, {});
+
+const collapsible = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsible, {});
+
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax, {});
