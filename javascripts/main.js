1188a9363ccaba40ffc5246d09eb8d6ccf59db7c
vidanmai <vidanmai@gmail.com>
2018-07-27 01:50:43 +0200

    Just changed a lot
     and added a lot to workout and videos

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,9 @@
 const sideNav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sideNav, {});
 
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
 const dropDown = document.querySelectorAll('.dropdown-trigger');
 M.Dropdown.init(dropDown, {});
 
@@ -9,3 +12,14 @@ M.Collapsible.init(collapsible, {});
 
 const parallax = document.querySelectorAll('.parallax');
 M.Parallax.init(parallax, {});
+
+const tabs = document.querySelectorAll('.tabs');
+M.Tabs.init(tabs, {
+    swipeable: true,
+});
+
+const fab = document.querySelector('.fixed-action-btn');
+M.FloatingActionButton.init(fab, {
+    direction: 'left'
+});
+
